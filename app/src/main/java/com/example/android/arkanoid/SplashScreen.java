ca0485ba2adb324c32153781d0943c1f98126eaf
gennaro-97 <gennaro-97@live.it>
2022-01-28 17:17:02 +0100

    closes #3

---
+++ app/src/main/java/com/example/android/arkanoid/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.android.arkanoid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainMenu.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
